6b79ab5d7af4aa9508d90439a68bf6b9f0a80a5c
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-16 17:47:30 -0500

    test

--- src/main/java/me/tony/main/vnskyblock/Administration/Commands.java
+++ src/main/java/me/tony/main/vnskyblock/Administration/Commands.java
@@ -1,4 +1,15 @@
 package me.tony.main.vnskyblock.Administration;
 
-public class Commands {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class Commands implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return true;
+    }
 }
