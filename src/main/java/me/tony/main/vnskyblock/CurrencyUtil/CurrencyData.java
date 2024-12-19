f28b7329e893e4b8068663cf69b03ccecb1306c3
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-19 05:09:13 -0500

    Fixed Loading Data

--- src/main/java/me/tony/main/vnskyblock/CurrencyUtil/CurrencyData.java
+++ src/main/java/me/tony/main/vnskyblock/CurrencyUtil/CurrencyData.java
@@ -2,36 +2,58 @@ package me.tony.main.vnskyblock.CurrencyUtil;
 
 import me.tony.main.vnskyblock.CurrencyUtil.PlayerData.GemConomy;
 import me.tony.main.vnskyblock.VNSkyblock;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
+import java.util.logging.Level;
 
 public class CurrencyData {
 
-    private File file;
-    private YamlConfiguration config;
+    private static File file;
+    private static YamlConfiguration config;
 
-    public void saveCurrency() {
+
+    public static void loadCurrency() {
+        file = new File(VNSkyblock.getInstance().getDataFolder(), "CurrencyData.yml");
+        new YamlConfiguration(); // not ideal, but works.
+        config = YamlConfiguration.loadConfiguration(file);
+
+        if (config.contains("player_currency")) {
+            for (String key : config.getConfigurationSection("player_currency").getKeys(false)) {
+                UUID uuid = UUID.fromString(key);
+                int balance = config.getInt("player_currency." + key);
+                GemConomy.PlayerData.put(uuid, balance);
+            }
+        } else {
+            Bukkit.getLogger().log(Level.SEVERE, "Config ERROR");
+        }
+    }
+
+
+    public static void saveCurrency() {
 
         file = new File(VNSkyblock.getInstance().getDataFolder(), "CurrencyData.yml");
         config = new YamlConfiguration();
+
+        config.createSection("player_currency");
 
         if (!GemConomy.PlayerData.isEmpty()) {
             for (UUID pUUID : GemConomy.PlayerData.keySet()) {
                 int amount = GemConomy.PlayerData.get(pUUID);
                 if (amount == 0) return;
-                config.set("player_currency.", pUUID.toString() + " : " + amount);
+                config.set("player_currency." + pUUID.toString(), amount);
             }
         }
+        Save();
 
     }
 
 
-    public void Save() {
+    public static void Save() {
 
         file = new File(VNSkyblock.getInstance().getDataFolder(), "CurrencyData.yml");
 
@@ -42,7 +64,7 @@ public class CurrencyData {
         }
     }
 
-    public void Load() throws IOException, InvalidConfigurationException {
+    public static void Load() throws IOException, InvalidConfigurationException {
         file = new File(VNSkyblock.getInstance().getDataFolder(), "CurrencyData.yml");
         config = new YamlConfiguration();
         config.options().copyDefaults(true);
