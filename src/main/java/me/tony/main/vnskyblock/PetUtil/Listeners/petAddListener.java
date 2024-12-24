af9028517965f820d22d4c6d5cbf169e41d781d4
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-24 16:16:14 -0500

    Cleaned up Pet Add Method.

--- src/main/java/me/tony/main/vnskyblock/PetUtil/Listeners/petAddListener.java
+++ src/main/java/me/tony/main/vnskyblock/PetUtil/Listeners/petAddListener.java
@@ -1,6 +1,9 @@
 package me.tony.main.vnskyblock.PetUtil.Listeners;
 
 import me.tony.main.vnskyblock.PetUtil.DataManagement.playerOwnedPets;
+import me.tony.main.vnskyblock.Util.debug;
+import me.tony.main.vnskyblock.VNSkyblock;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,20 +11,28 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+
 public class petAddListener implements Listener {
 
     @EventHandler
     public void petClaim(PlayerInteractEvent e) {
+
+        List<String> serverPets = VNSkyblock.getInstance().getConfig().getStringList("pet_names");
+
         Player p = e.getPlayer();
         ItemStack item = p.getInventory().getItemInMainHand();
         Action a = e.getAction();
 
         if (a.equals(Action.RIGHT_CLICK_AIR) || a.equals(Action.RIGHT_CLICK_BLOCK)) {
-            String strippedColor = item.getItemMeta().getDisplayName().strip();
-            if (strippedColor.contains("Rock") || strippedColor.contains("Monkey")) {
-                playerOwnedPets.addPet(p, item);
-            } else {
-                System.out.println("false");
+            if (item.getItemMeta() == null || item.getType().equals(Material.AIR)) return;
+            String strippedColor = item.getItemMeta().getDisplayName().strip().toLowerCase();
+            for (String list : serverPets) {
+                if (strippedColor.contains(list)) {
+                    e.setCancelled(true);
+                    playerOwnedPets.addPet(p, item);
+                    p.getInventory().remove(item);
+                }
             }
         }
 
