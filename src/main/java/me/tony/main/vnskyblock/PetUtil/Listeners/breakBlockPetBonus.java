b3943b0b91353dc3bf31da903ee4c73a11db225f
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-25 23:22:38 -0500

    Added Timber Ability, and Rarities

--- src/main/java/me/tony/main/vnskyblock/PetUtil/Listeners/breakBlockPetBonus.java
+++ src/main/java/me/tony/main/vnskyblock/PetUtil/Listeners/breakBlockPetBonus.java
@@ -1,12 +1,21 @@
 package me.tony.main.vnskyblock.PetUtil.Listeners;
 
+import me.tony.main.vnskyblock.PetUtil.DataManagement.petBonus;
 import me.tony.main.vnskyblock.PetUtil.DataManagement.playerOwnedPets;
 import me.tony.main.vnskyblock.Util.debug;
+import me.tony.main.vnskyblock.Util.randomChance;
+import me.tony.main.vnskyblock.VNSkyblock;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class breakBlockPetBonus implements Listener {
 
@@ -15,10 +24,55 @@ public class breakBlockPetBonus implements Listener {
         Player p = e.getPlayer();
         Block b = e.getBlock();
 
-        if (playerOwnedPets.getActiveBonus(p) == null);
+        List<String> blockCheck = VNSkyblock.getInstance().getConfig().getStringList("log_check");
 
-        debug.print(playerOwnedPets.getActiveBonus(p));
+        if (petBonus.getBonus(p) == null) return;
+
+        debug.print(petBonus.getBonus(p));
+        if (petBonus.getBonus(p).contains("LOGGING")) {
+            if (petBonus.getBonus(p).contains("COMMON") || petBonus.getBonus(p).contains("RARE") || petBonus.getBonus(p).contains("LEGENDARY")) {
+                for (String s : blockCheck) {
+                    if (b.getType().equals(Material.valueOf(s))) {
+                        if (randomChance.chance(randomChance.randomNumber(1, 100), 25)) {
+                            b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(b.getType(), randomChance.randomNumber(1, 3)));
+                        }
+                    }
+                }
+            }
+            if (petBonus.getBonus(p).contains("RARE") || petBonus.getBonus(p).contains("LEGENDARY")) {
+                for (String s : blockCheck) {
+                    if (b.getType().equals(Material.valueOf(s))) {
+                        if (randomChance.chance(randomChance.randomNumber(1, 100), 15)) {
+                            Set<Block> blockToBreak = new HashSet<>();
+                            timberAbility(b, Material.valueOf(s), blockToBreak, 5);
+                            for (Block toBreak : blockToBreak) {
+                                toBreak.breakNaturally();
+                            }
+                       }
+                    }
+                }
+            }
+        }
     }
 
 
+    private void timberAbility(Block b, Material material, Set<Block> blockToBreak, int limit) {
+        if (blockToBreak.size() >= limit) return;
+        if (!blockToBreak.contains(b) && b.getType() == material) {
+            blockToBreak.add(b);
+
+            timberAbility(b.getRelative(1, 0, 0), material, blockToBreak, limit);
+            timberAbility(b.getRelative(-1, 0, 0), material, blockToBreak, limit);
+            timberAbility(b.getRelative(0, 1, 0), material, blockToBreak, limit);
+            timberAbility(b.getRelative(0, -1, 0), material, blockToBreak, limit);
+            timberAbility(b.getRelative(0, 0, 1), material, blockToBreak, limit);
+            timberAbility(b.getRelative(0, 0, -1), material, blockToBreak, limit);
+
+        }
+    }
+
+
+
 }
+
+
