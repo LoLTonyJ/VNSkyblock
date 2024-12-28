ce4f3351c29a2decaeec5ecc7e3c92295b7faa4e
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-28 00:54:27 -0500

    Implemented Levels

---
+++ src/main/java/me/tony/main/vnskyblock/PlayerLevel/chatFormat.java
@@ -0,0 +1,23 @@
+package me.tony.main.vnskyblock.PlayerLevel;
+
+import me.tony.main.vnskyblock.Util.chatUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class chatFormat implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        Player p = e.getPlayer();
+        String msg = e.getMessage();
+        int level = playerManager.getLevel(p);
+
+        e.setFormat(chatUtil.format("&7[&b⭐" + level + "&7] " + p.getDisplayName() + " &7> " + msg));
+
+    }
+
+
+
+}
