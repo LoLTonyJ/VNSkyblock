3da2714a81a438157754564bbd967056df77a6cb
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-28 00:51:39 -0500

    Used for petItems UUID

---
+++ src/main/java/me/tony/main/vnskyblock/PDC/Keys.java
@@ -0,0 +1,10 @@
+package me.tony.main.vnskyblock.PDC;
+
+import me.tony.main.vnskyblock.VNSkyblock;
+import org.bukkit.NamespacedKey;
+
+public class Keys {
+
+    public static final NamespacedKey UNIQUE_IDENTIFIER = new NamespacedKey(VNSkyblock.getInstance(), "UUID");
+
+}
