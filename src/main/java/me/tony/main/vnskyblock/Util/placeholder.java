a10632326ad0e1d334c4fe0e053e4cb666733baa
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2024-12-28 00:53:02 -0500

    Added List<String> replacement

--- src/main/java/me/tony/main/vnskyblock/Util/placeholder.java
+++ src/main/java/me/tony/main/vnskyblock/Util/placeholder.java
@@ -1,9 +1,21 @@
 package me.tony.main.vnskyblock.Util;
 
+import java.util.List;
+
 public class placeholder {
 
     public static String replace(String placeholder, String replaceWith, String msg) {
         return msg.replace(placeholder, replaceWith);
+    }
+
+    public static List<String> replaceListPlaceholder(List<String> list, String placeholder, String toReplace) {
+
+        List<String> checkList = list;
+        for (String s : checkList) {
+            String replacedString = s.replace(placeholder, toReplace);
+            checkList.add(replacedString);
+        }
+        return checkList;
     }
 
 
