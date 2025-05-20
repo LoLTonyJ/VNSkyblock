b11de46b9ee4b928797e1eadc157882fc58d45f8
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2025-05-20 03:34:04 -0400

    Added "BOTH" as a configuration option. Added getTemplateList() method.

--- src/main/java/me/tony/main/vnskyblock/Admin/FileManipulation/punishConfiguration.java
+++ src/main/java/me/tony/main/vnskyblock/Admin/FileManipulation/punishConfiguration.java
@@ -1,12 +1,19 @@
 package me.tony.main.vnskyblock.Admin.FileManipulation;
 
 import me.tony.main.vnskyblock.VNSkyblock;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class punishConfiguration {
 
@@ -15,6 +22,24 @@ public class punishConfiguration {
 
     public static List<String> getReasonBuilder(String reason) {
         return config.getStringList("ban_reason_replace." + reason.toLowerCase());
+    }
+
+    public static List<ItemStack> getTemplateList() {
+        ConfigurationSection templateSection = config.getConfigurationSection("ban_reason_replace");
+        Set<String> keys = templateSection.getKeys(false);
+        List<ItemStack> itemStacks = new ArrayList<>();
+        for (String key : keys) {
+            ItemStack item = new ItemStack(Material.PAPER);
+            ItemMeta meta = item.getItemMeta();
+            if (meta != null) {
+                meta.setDisplayName(ChatColor.GRAY + key);
+                List<String> lore = templateSection.getStringList(key);
+                meta.setLore(lore);
+                item.setItemMeta(meta);
+            }
+            itemStacks.add(item);
+        }
+        return itemStacks;
     }
 
     public static void reloadPunishConfig() throws IOException, InvalidConfigurationException {
@@ -27,6 +52,14 @@ public class punishConfiguration {
         }
         if (getSetLink().equalsIgnoreCase("WEBSITE")) {
             return (String) config.get("website_invite_link");
+        }
+        if (getSetLink().equalsIgnoreCase("BOTH")) {
+            ArrayList<String> links = new ArrayList<>();
+            links.add((String) config.get("discord_invite_link"));
+            links.add((String) config.get("website_invite_link"));
+            for (String s : links) {
+                return s;
+            }
         }
         return "INVITE_NULL";
     }
