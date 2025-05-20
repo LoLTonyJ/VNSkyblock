181f36e311d98c4f035404ea1ace0570086900ad
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2025-05-20 03:35:44 -0400

    Finished Island Level getter.

--- src/main/java/me/tony/main/vnskyblock/PlayerData/islandLevel.java
+++ src/main/java/me/tony/main/vnskyblock/PlayerData/islandLevel.java
@@ -1,13 +1,13 @@
 package me.tony.main.vnskyblock.PlayerData;
 
-import me.tony.main.vnskyblock.IslandUtil.PlayerManager;
+import me.tony.main.vnskyblock.IslandUtil.IslandPlayerManager;
 import org.bukkit.entity.Player;
 
 public class islandLevel {
 
     public static Long getLevel(Player p) {
-        if (PlayerManager.hasIsland(p)) {
-            return PlayerManager.getIslandLevel(p.getUniqueId());
+        if (IslandPlayerManager.hasIsland(p)) {
+            return IslandPlayerManager.getIslandLevel(p.getUniqueId());
         }
         return null;
     }
