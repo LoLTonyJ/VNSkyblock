a1a6d7683f4b5e845bef4c1e1f57242db7723914
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2025-05-21 06:03:17 -0400

    Added File Getters/Inputters

--- src/main/java/me/tony/main/vnskyblock/Admin/PlayerManager/PunishManager.java
+++ src/main/java/me/tony/main/vnskyblock/Admin/PlayerManager/PunishManager.java
@@ -1,64 +1,106 @@
 package me.tony.main.vnskyblock.Admin.PlayerManager;
 
-import me.tony.main.vnskyblock.Admin.FileManipulation.bannedPlayers;
+
 import me.tony.main.vnskyblock.Util.ChatColor;
 import me.tony.main.vnskyblock.VNSkyblock;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
-public class PunishManager implements Listener {
+public class PunishManager {
 
-    public static ArrayList<Player> frozenPlayers = new ArrayList<>();
+    private static File file;
+    private static YamlConfiguration config;
 
-    @EventHandler
-    public static void onLeave(PlayerQuitEvent e) {
-        boolean banOnLeave = VNSkyblock.getInstance().getConfig().getBoolean("ban_on_leave");
-        List<String> banReason = VNSkyblock.getInstance().getConfig().getStringList("ban_message");
-        Player p = e.getPlayer();
+    public static boolean isBanned(Player player) {
+        Set<String> bannedPlayers = config.getKeys(false);
+        for (String playerName : bannedPlayers) {
+            return playerName.equalsIgnoreCase(player.getName());
+        }
+        return false;
+    }
 
-        if (isFrozen(p)) {
-            bannedPlayers.addPlayerBan(p, "Left Whilst Frozen", banReason);
+    public static void removeBan(Player staff, Player victim) {
+        if (!isBanned(victim.getPlayer())) {
+            staff.sendMessage(ChatColor.format("&c&l!! &cThat player is not banned!"));
+            return;
+        }
+        config.set(victim.getName(), null);
+        staff.sendMessage(ChatColor.format("&aUnbanned " + victim.getName()));
+        Save();
+    }
+
+    public static void addBan(Player staff, OfflinePlayer victim, List<String> reason) {
+        if (victim == null) {
+            System.out.println("NULL PLAYER");
+            return;
+        }
+        if (isBanned(victim.getPlayer())) {
+            staff.sendMessage(ChatColor.format("&c&l!! &cThat player is already banned!"));
+            staff.closeInventory();
+        }
+        config.set(victim.getName(), null);
+        config.set(victim.getName() + ".UUID", victim.getUniqueId().toString());
+        config.set(victim.getName() + ".Reason", reason);
+        config.set(victim.getName() + ".Banned By", staff.getName());
+
+        Save();
+    }
+
+    public static void addHistoryInput(Player staff, OfflinePlayer victim, String punishment, List<String> reason) {
+        if (victim == null) {
+            System.out.println("NULL PLAYER");
+            return;
+        }
+        // To be continued.
+        config.set(victim.getName(), null);
+        config.set(victim.getName() + ".Punishment Type", punishment.toUpperCase());
+        config.set(victim.getName() + ".Reason", reason);
+        config.set(victim.getName() + ".Responsible Staff Member", staff.getName());
+        saveHistoryFile();
+    }
+
+    public static void loadHistoryFile() throws IOException, InvalidConfigurationException {
+        file = new File(VNSkyblock.getInstance().getDataFolder(), "PunishedPlayers/PlayerHistory/PlayerHistoryDir.yml");
+        YamlConfiguration historyConfig = new YamlConfiguration();
+        historyConfig.options().copyDefaults(true);
+        if (!file.exists()) VNSkyblock.getInstance().saveResource("PunishedPlayers/PlayerHistory/PlayerHistoryDir.yml", false);
+        config.load(file);
+    }
+
+    public static void saveHistoryFile() {
+        file = new File(VNSkyblock.getInstance().getDataFolder(), "PunishedPlayers/PlayerHistory/PlayerHistoryDir.yml");
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    @EventHandler
-    public static void onMove(PlayerMoveEvent e) {
+    public static void Save() {
 
-        List<String> frozenMessage = VNSkyblock.getInstance().getConfig().getStringList("frozen_message_player");
+        file = new File(VNSkyblock.getInstance().getDataFolder(), "PunishedPlayers/banned_players.yml");
 
-        Player p = e.getPlayer();
-        if (isFrozen(p)) {
-            for (String s : frozenMessage) {
-                p.sendMessage(ChatColor.format(s));
-            }
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 
+    public static void Load() throws IOException, InvalidConfigurationException {
+        file = new File(VNSkyblock.getInstance().getDataFolder(), "PunishedPlayers/banned_players.yml");
+        config = new YamlConfiguration();
+        config.options().copyDefaults(true);
+        if (!file.exists()) VNSkyblock.getInstance().saveResource("PunishedPlayers/banned_players.yml", false);
+        config.load(file);
 
-    public static boolean isFrozen(Player player) {
-        return frozenPlayers.contains(player);
-    }
-
-    public static void freezePlayer(Player player, Player staff) {
-
-        List<String> frozenMessage = VNSkyblock.getInstance().getConfig().getStringList("frozen_message_player");
-
-        if (frozenPlayers.contains(player)) {
-            frozenPlayers.remove(player);
-            staff.sendMessage(ChatColor.format("&aYou have unfrozen " + player.getDisplayName()));
-        }
-        frozenPlayers.add(player);
-        staff.sendMessage(ChatColor.format("&cYou have frozen " + player.getDisplayName()));
-        for (String s : frozenMessage) {
-            player.sendMessage(ChatColor.format(s));
-        }
     }
 
 }
