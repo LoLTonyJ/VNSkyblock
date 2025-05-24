20077085b8b2c48e28d0d693ee5a9351dac4782c
SturdySteady <106615272+SturdySteady@users.noreply.github.com>
2025-05-24 04:55:30 -0400

    Added Mute, and Kick GUI functionality.

--- src/main/java/me/tony/main/vnskyblock/Admin/GUI/Events/ClickInventory.java
+++ src/main/java/me/tony/main/vnskyblock/Admin/GUI/Events/ClickInventory.java
@@ -2,8 +2,11 @@ package me.tony.main.vnskyblock.Admin.GUI.Events;
 
 import me.tony.main.vnskyblock.Admin.FileManipulation.punishConfiguration;
 import me.tony.main.vnskyblock.Admin.GUI.PunishGUI;
+import me.tony.main.vnskyblock.Admin.PlayerManager.MuteManager;
 import me.tony.main.vnskyblock.Admin.PlayerManager.PunishManager;
 import me.tony.main.vnskyblock.Util.ChatColor;
+import me.tony.main.vnskyblock.Util.permCheck;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -36,7 +39,45 @@ public class ClickInventory implements Listener {
             String itemName = org.bukkit.ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
             List<String> reason = punishConfiguration.getReasonBuilder("ban_reason_replace", itemName);
             PunishManager.addBan(p, victim, reason);
+            PunishManager.kickBanPlayer(victim);
+            for (Player onlineStaff : Bukkit.getOnlinePlayers()) {
+                if (permCheck.isHelper(onlineStaff)) {
+                    if (!PunishManager.logsToggled(onlineStaff)) {
+                        onlineStaff.sendMessage(ChatColor.format(PunishManager.sendLogInformation(p, victim, "Banned")));
+                    }
+                }
+            }
             p.closeInventory();
+        }
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.format(punishConfiguration.getConfigPathString("kick_gui_name")))) {
+            e.setCancelled(true);
+            ItemStack clickedItem = e.getCurrentItem();
+            if (clickedItem.getItemMeta() == null) return;
+            String itemName = org.bukkit.ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
+            List<String> reason = punishConfiguration.getReasonBuilder("kick_reason_replace", itemName);
+            PunishManager.kickPlayer(victim, reason);
+            for (Player onlineStaff : Bukkit.getOnlinePlayers()) {
+                if (permCheck.isHelper(onlineStaff)) {
+                    if (!PunishManager.logsToggled(onlineStaff)) {
+                        onlineStaff.sendMessage(ChatColor.format(PunishManager.sendLogInformation(p, victim, "Kicked")));
+                    }
+                }
+            }
+        }
+        if (e.getView().getTitle().equalsIgnoreCase(ChatColor.format(punishConfiguration.getConfigPathString("mute_gui_name")))) {
+            e.setCancelled(true);
+            ItemStack clickedItem = e.getCurrentItem();
+            if (clickedItem.getItemMeta() == null) return;
+            String itemName = org.bukkit.ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
+            List<String> reason = punishConfiguration.getReasonBuilder("mute_reason_replace", itemName);
+            MuteManager.setMuted(p, victim, reason);
+            for (Player onlineStaff : Bukkit.getOnlinePlayers()) {
+                if (permCheck.isHelper(onlineStaff)) {
+                    if (!PunishManager.logsToggled(onlineStaff)) {
+                        onlineStaff.sendMessage(ChatColor.format(PunishManager.sendLogInformation(p, victim, "Muted")));
+                    }
+                }
+            }
         }
 
         if (e.getView().getTitle().equalsIgnoreCase(ChatColor.format(punishConfiguration.getConfigPathString("main_punish_gui_name")))) {
